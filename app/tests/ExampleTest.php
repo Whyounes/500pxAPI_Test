c52ed9e5632440f2a413bd1fc2e0461b8740b535
RAFIE Younes <younes.rafie@gmail.com>
2017-03-17 09:49:09 +0000

    Removed test

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -9,9 +9,7 @@ class ExampleTest extends TestCase {
 	 */
 	public function testBasicExample()
 	{
-		$crawler = $this->client->request('GET', '/');
 
-		$this->assertTrue($this->client->getResponse()->isOk());
 	}
 
 }
