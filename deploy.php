468103b4fb70cd0a2378c37986959dfc23517cf8
RAFIE Younes <younes.rafie@gmail.com>
2017-03-17 11:34:24 +0000

    Updated composer options to deploy

--- deploy.php
+++ deploy.php
@@ -5,6 +5,7 @@ require_once "recipe/common.php";
 set('ssh_type', 'native');
 set('default_stage', 'staging');
 env('deploy_path', '/var/www');
+env('composer_options', 'update --no-dev --prefer-dist --no-progress --no-interaction');
 set('copy_dirs', [
     'app/commands',
     'app/config',
